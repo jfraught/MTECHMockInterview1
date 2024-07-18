90289ab6d219978a3cadc86b839685162c8640c0
Jordan Fraughton <fraughtdawg@gmail.com>
2024-07-18 08:34:41 -0600

    Display users

---
+++ ChooseRandomUsers/Models/User.swift
@@ -0,0 +1,42 @@
+//
+//  User.swift
+//  ChooseRandomUsers
+//
+//  Created by Jordan Fraughton on 7/18/24.
+//
+
+import Foundation
+
+struct User {
+    var id = UUID()
+    var name: String
+}
+
+extension User: Codable, Equatable {
+    static let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let archiveURL = documentsDirectory.appendingPathComponent("users").appendingPathExtension("plist")
+    
+    static func ==(lhs: User, rhs: User) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    static func loadUsers() -> [User]? {
+        guard let codedUsers = try? Data(contentsOf: archiveURL) else { return nil }
+        
+        return try? PropertyListDecoder().decode(Array<User>.self, from: codedUsers)
+    }
+    
+    static func saveUsers(_ users: [User]) {
+        let codedUsers = try? PropertyListEncoder().encode(users)
+        try? codedUsers?.write(to: archiveURL, options: .noFileProtection)
+    }
+    
+    static func loadSampleUsers() -> [User] {
+        let userOne = User(name: "Jordan")
+        let userTwo = User(name: "Juli")
+        let userThree = User(name: "Allie")
+        let userFour = User(name: "William")
+        
+        return [userOne, userTwo, userThree, userFour]
+    }
+}
--- ChooseRandomUsers/View Controllers/UsersTableViewController.swift
+++ ChooseRandomUsers/View Controllers/UsersTableViewController.swift
@@ -8,38 +8,38 @@
 import UIKit
 
 class UsersTableViewController: UITableViewController {
-
+    
+    var users = [User]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        if let savedUsers = User.loadUsers() {
+            users = savedUsers
+        } else {
+            users = User.loadSampleUsers()
+        }
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        users.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath)
+        
+        let user = users[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = user.name
+        cell.contentConfiguration = content
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
